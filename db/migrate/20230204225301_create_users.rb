a7b308104f0cc635155e00e0472e39ba34cdac34
Kiyo-18 <johnlloyd.desape@evsu.edu.ph>
2023-02-05 06:54:15 +0800

    bundle install authentication-zero

---
+++ db/migrate/20230204225301_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :email,           null: false
+      t.string :password_digest, null: false
+
+      t.boolean :verified, null: false, default: false
+
+      t.timestamps
+    end
+
+    add_index :users, :email, unique: true
+  end
+end
