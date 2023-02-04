46a6b0f4bcbf09d3e31541f7c39ad8a605d01085
Kiyo-18 <johnlloyd.desape@evsu.edu.ph>
2023-02-05 06:54:15 +0800

    bundle install authentication-zero

---
+++ README.md
@@ -0,0 +1,20 @@
+## Installation 
+
+`$ bundle add authentication-zero`
+
+
+## Usage
+`$ rails generate authentication`
+
+## Steps
+`Rails db:migrate`
+
+
+
+
+
+
+
+
+### Source 
+https://github.com/lazaronixon/authentication-zero
---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  get  'sign_in', to: 'sessions#new'
+  post 'sign_in', to: 'sessions#create'
+  get  'sign_up', to: 'registrations#new'
+  post 'sign_up', to: 'registrations#create'
+  resources :sessions, only: [:index, :show, :destroy]
+  resource  :password, only: [:edit, :update]
+  namespace :identity do
+    resource :email,              only: [:edit, :update]
+    resource :email_verification, only: [:edit, :create]
+    resource :password_reset,     only: [:new, :edit, :create, :update]
+  end
+  root 'home#index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
